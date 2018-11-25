44890fd92f8e3fdbf46dfd0b0e26d2eb601a62bf
phurinaix <phurinaix@gmail.com>
2018-11-26 05:49:36 +0700

    validate join

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');  
 
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 5000;
 var app = express();
@@ -20,6 +21,13 @@ io.on('connection', (socket) => {
 
     // socket.broadcast.emit from Admin text New user joined
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+
+    socket.on('join', (params, callback) => {
+        if (!isRealString(params.name) || !isRealString(params.pass)) {
+            callback('Name and room name are required.')
+        }
+        callback();
+    });
 
     socket.on('createMessage', (message, callback) => {
         console.log('create message', message);
