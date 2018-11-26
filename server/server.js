09a5587d2d87a8bb6dc2474176b9f6cb1251a9c4
phurinaix <phurinaix@gmail.com>
2018-11-27 04:57:32 +0700

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -23,14 +23,15 @@ io.on('connection', (socket) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
             return callback('Name and room name are required.')
         }
+
+        /*
+        if () {
+            เช็คชื่อซ้ำ
+        }
+        */
         socket.join(params.room);
         users.removeUser(socket.id);
         users.addUser(socket.id, params.name, params.room);
-        // socket.leave('The Office Fans');
-        
-        // io.emit -> io.to('The Office Fans').emit
-        // socket.broadcast.emit -> socket.broadcast.to('The Office Fans').emit
-        // socket.emit
 
         io.to(params.room).emit('updateUserList', users.getUserList(params.room));
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
@@ -40,13 +41,20 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('create message', message);
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
         callback();
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('createEmail', (newEmail) => {
